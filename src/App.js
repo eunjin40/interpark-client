4a692c33b99f80817bd0a5a2c039f17f6ecc1d19
eunjin40 <dev.eunjin40@gmail.com>
2023-11-02 14:08:15 +0900

    feat : visual, recommend 생성

--- src/App.js
+++ src/App.js
@@ -1,18 +1,21 @@
 import Header from "./components/Header";
+import Recommend from "./components/Recommend";
+import Visual from "./components/Visual";
 
 function App() {
   return (
     <div className="wrap">
-        {/* 상단 영역 */}
-        <Header />
-        
-        {/* 메인 영역 */}
-        <div className="main">
-        </div>
-        
-        {/* 하단 영역 */}
-        <footer className="footer">
-        </footer>
+      {/* 상단 영역 */}
+      <Header />
+
+      {/* 메인 영역 */}
+      <div className="main">
+        <Visual />
+        <Recommend />
+      </div>
+
+      {/* 하단 영역 */}
+      <footer className="footer"></footer>
     </div>
   );
 }
